e53bf6d379b5600ae8e9dfc6e66927ceb3f4e7db
EC2 Default User <ec2-user@ip-172-31-28-13.us-east-2.compute.internal>
2020-06-17 10:24:38 +0000

    課題の提出 18

---
+++ resources/views/profile/index.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.profile_front')
+
+@section('content')
+    <div class="container">
+        <hr color="#c0c0c0">
+        @if (!is_null($headline))
+            <div class="row">
+                <div class="headline col-md-10 mx-auto">
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="caption ">
+                                <div class="name p-2">
+                                    <h1>{{ str_limit($headline->name, 70) }}</h1>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="body mx-auto">・性別<br>{{ str_limit($headline->gender, 10) }}</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="body mx-auto">・趣味<br>{{ str_limit($headline->hobby, 650) }}</p>
+                        </div>
+                        <div class="col-md-12">
+                            <p class="body mx-auto">・自己紹介欄<br>{{ str_limit($headline->introduction, 650) }}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        @endif
+        <hr color="#c0c0c0">
+        <div class="row">
+            <div class="posts col-md-8 mx-auto mt-3">
+                @foreach($posts as $post)
+                    <div class="post">
+                        <div class="row">
+                            <div class="text col-md-6">
+                                <div class="date">
+                                    {{ $post->updated_at->format('Y年m月d日') }}
+                                </div>
+                                <div class="name">
+                                    {{ str_limit($post->name, 150) }}
+                                </div>
+                                <div class="body mt-3">
+                                    ・性別<br>{{ str_limit($post->gender, 1500) }}
+                                </div>
+                                <div class="body mt-3">
+                                    ・趣味<br>{{ str_limit($post->hobby, 1500) }}
+                                </div>
+                                <div class="body mt-3">
+                                    ・自己紹介欄<br>{{ str_limit($post->introduction, 1500) }}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <hr color="#c0c0c0">
+                @endforeach
+            </div>
+        </div>
+    </div>
+    </div>
+@endsection
